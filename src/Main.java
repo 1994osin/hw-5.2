da80fea75cf8a61e76e7332a7a05474f5589f7c7
Denis <1994osin@gmail.com>
2022-11-10 11:57:52 +0300

    Сделанное ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Task 1
+        for (int i = 1904; i < 2096; i++) {
+            if ((i % 4 == 0 && i % 100 != 0) || (i % 400 == 0))
+                System.out.println(i + " год является високосным");
+        }
+
+        // Task 2
+        for (int i = 7; i <= 98; i += 7) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        // Task 3
+        for (int i = 1; i <= 512; i *= 2) {
+            System.out.print(i + " ");
+        }
     }
 }
